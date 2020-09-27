f64022d1f83165d7f61e2066d231ffb9d85ffdaf
yosephNoh <yoseph.noh@solarconnect.kr>
2020-09-28 08:58:20 +0900

    repository 분리

--- eatgo-api/src/main/java/com/playground/noyo0123/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/playground/noyo0123/eatgo/interfaces/RestaurantController.java
@@ -1,22 +1,22 @@
 package com.playground.noyo0123.eatgo.interfaces;
 
 import com.playground.noyo0123.eatgo.domain.Restaurant;
+import com.playground.noyo0123.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
 
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
     }
@@ -24,17 +24,7 @@ public class RestaurantController {
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable Long id) {
 
-        List<Restaurant> restaurants = new ArrayList<>();
-
-
-        restaurants.add(new Restaurant(1004L,"Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-
+        Restaurant restaurant = repository.findById(id);
 
         return restaurant;
     }
