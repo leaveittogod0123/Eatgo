afdc648779feed308809b58e400a047f7bf2532b
yosephNoh <yoseph.noh@solarconnect.kr>
2020-11-02 08:31:04 +0900

    h2 파일 저장 영속화

--- eatgo-admin-api/src/main/java/com/playground/noyo0123/eatgo/interfaces/MenuItemController.java
+++ eatgo-admin-api/src/main/java/com/playground/noyo0123/eatgo/interfaces/MenuItemController.java
@@ -3,12 +3,8 @@ package com.playground.noyo0123.eatgo.interfaces;
 import com.playground.noyo0123.eatgo.application.MenuItemService;
 import com.playground.noyo0123.eatgo.domain.MenuItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,6 +12,13 @@ public class MenuItemController {
 
     @Autowired
     private MenuItemService menuItemService;
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> list(@PathVariable Long restaurantId) {
+        List<MenuItem> menuItems = menuItemService.getMenuItems(restaurantId);
+
+        return menuItems;
+    }
 
     @PatchMapping("/restaurants/{restaurantId}/menuitems")
     public String bulkUpdate(
--- eatgo-common/src/main/java/com/playground/noyo0123/eatgo/domain/MenuItemRepository.java
+++ eatgo-common/src/main/java/com/playground/noyo0123/eatgo/domain/MenuItemRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
-
     List<MenuItem> findAllByRestaurantId(Long id);
-
     void deleteById(Long id);
-
 }
