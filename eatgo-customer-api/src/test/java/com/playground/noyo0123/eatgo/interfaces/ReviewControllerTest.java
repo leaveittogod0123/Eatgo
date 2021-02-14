11bd5186f0be505ab3f39fb18aa4ea074fea17d7
yosephNoh <yoseph.noh@solarconnect.kr>
2021-02-15 00:17:14 +0900

    jwt token으로 review 남기기

--- eatgo-customer-api/src/test/java/com/playground/noyo0123/eatgo/interfaces/ReviewControllerTest.java
+++ eatgo-customer-api/src/test/java/com/playground/noyo0123/eatgo/interfaces/ReviewControllerTest.java
@@ -33,7 +33,8 @@ public class ReviewControllerTest {
 
     @Test
     public void createWithValidAttribute() throws Exception {
-        given(reviewService.addReview(eq(1L),any())).willReturn(Review.builder()
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEwMDQsIm5hbWUiOiJKb2huIn0.8hm6ZOJykSINHxL-rf0yV882fApL3hyQ9-WGlJUyo2A";
+        given(reviewService.addReview(1L, "John", 3, "Mat-it-da")).willReturn(Review.builder()
                 .id(1004L)
                 .name("JOKER")
                 .score(3)
@@ -42,11 +43,12 @@ public class ReviewControllerTest {
 
         mvc.perform(post("/restaurants/1/reviews")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"JOKER\", \"score\" :3, \"description\":\"Mat-it-da\"}"))
-                .andExpect(header().string("location", "/restaurants/1/reviews/1004"))
-                .andExpect(status().isCreated());
+                .header("Authorization", "Bearer "+ token)
+                .content("{\"score\" :3, \"description\":\"Mat-it-da\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("location", "/restaurants/1/reviews/1004"));
 
-        verify(reviewService).addReview(eq(1L), any());
+        verify(reviewService).addReview(eq(1L), eq("John"), eq(3), eq("Mat-it-da"));
     }
 
     @Test
@@ -56,6 +58,6 @@ public class ReviewControllerTest {
                 .content("{}"))
                 .andExpect(status().isBadRequest());
 
-        verify(reviewService, never()).addReview(eq(1L), any());
+        verify(reviewService, never()).addReview(any(), any(), any(),any());
     }
 }
