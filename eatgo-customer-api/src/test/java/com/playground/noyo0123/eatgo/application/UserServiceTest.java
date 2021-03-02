64f54246c8343ca268385e9a15417e1495f360b7
yosephNoh <yoseph.noh@solarconnect.kr>
2021-03-03 07:54:10 +0900

    reservation controller 개발

--- eatgo-customer-api/src/test/java/com/playground/noyo0123/eatgo/application/UserServiceTest.java
+++ eatgo-customer-api/src/test/java/com/playground/noyo0123/eatgo/application/UserServiceTest.java
@@ -60,42 +60,4 @@ public class UserServiceTest {
 
         verify(userRepository, never()).save(any());
     }
-
-    @Test
-    public void authenticateWithValidAttributes() {
-        String email = "tester@example.com";
-        String password = "test";
-
-        User mockUser = User.builder().email(email).build();
-        given(userRepository.findByEmail(email)).willReturn(Optional.of(mockUser));
-
-        given(passwordEncoder.matches(any(), any())).willReturn(true);
-
-        User user = userService.authenticate(email, password);
-        assertThat(user.getEmail(), is(email)); // org.junit is.is
-    }
-
-    @Test(expected = EmailNotExistedException.class)
-    public void authenticateWithNotExistedEmail() {
-        String email = "x@example.com";
-        String password = "test";
-
-        given(userRepository.findByEmail(email)).willReturn(Optional.empty());
-
-        userService.authenticate(email, password);
-    }
-
-    @Test(expected = PasswordWrongException.class)
-    public void authenticateWithWrongPassword() {
-        String email = "x@example.com";
-        String password = "test";
-
-        User mockUser = User.builder().email(email).build();
-
-        given(userRepository.findByEmail(email)).willReturn(Optional.of(mockUser));
-
-        given(passwordEncoder.matches(any(), any())).willReturn(false);
-
-        userService.authenticate(email, password);
-    }
 }
