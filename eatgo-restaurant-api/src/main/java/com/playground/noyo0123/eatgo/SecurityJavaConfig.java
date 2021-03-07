9da787c1447e34c0fb3447ba31d3cf0275dc6359
yosephNoh <yoseph.noh@solarconnect.kr>
2021-03-08 00:48:34 +0900

    가게 주인 예약 조회 API 구현

---
+++ eatgo-restaurant-api/src/main/java/com/playground/noyo0123/eatgo/SecurityJavaConfig.java
@@ -0,0 +1,51 @@
+package com.playground.noyo0123.eatgo;
+
+import com.playground.noyo0123.eatgo.filters.JwtAuthenticationFilter;
+import com.playground.noyo0123.eatgo.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.secret}")
+    private String secret; // application.yml
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        Filter filter = new JwtAuthenticationFilter(authenticationManager(), jwtUtil());
+
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .cors().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .addFilter(filter)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 정책
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+
+        return new JwtUtil(secret);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
